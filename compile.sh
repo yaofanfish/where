71fd87b91f238a2e3a5e9d99cd6c646bc8d84299
yaofanfish <yaofanfish@gmail.com>
2026-03-28 08:55:35 +0000

    idk

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -x
 # python--\>c
-cython --embed where.pyx -o .where.c.c
+cd build
+cython --embed ../where.pyx -o where.c
 # echo c--\>binary
-gcc -Ofast $(python3-config --includes) .where.c.c $(python3-config --libs) -lpython3.14 -lpthread -lm -lutil -ldl -o build/where
+gcc -Ofast $(python3-config --includes) where.c $(python3-config --libs) -lpython3.14 -lpthread -lm -lutil -ldl -o where
 
 
